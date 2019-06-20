6c44f7d5e2c3ad83af8558bac6cc21dc71d14207
omadebaba <iamoyemadeteepee@gmail.com>
2019-06-20 22:11:34 +0100

    Added exercise6.17

---
+++ Cohort1/Tope/Chapter6/src/chapter6_exercises/SquareofAsteriks.java
@@ -0,0 +1,34 @@
+package chapter6_exercises;
+
+import java.util.Scanner;
+
+public class SquareofAsteriks {
+//Exercise 6.17
+//Program to display square of asteriks
+//Written by Oyemade Temitope
+//20th of June 2019
+
+	
+	public static void squareOfAsteriks(int side) {
+		
+		for(int i = 1; i<=Math.pow(side, 2); i++) {
+			System.out.print("*");
+			
+			if(i % side ==0 )
+				System.out.println();
+			
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+			System.out.println("Enter number");
+			int number = input.nextInt();
+			squareOfAsteriks(number);
+			input.close();
+	}
+
+}
