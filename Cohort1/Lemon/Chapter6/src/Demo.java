ce3b68fc23d52bcec5d97002e4c8e94e66669939
LEMON <joshval.lj@gmail.com>
2019-06-20 13:53:28 -0700

    pair programming with gbemi

---
+++ Cohort1/Lemon/Chapter6/src/Demo.java
@@ -0,0 +1,13 @@
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			double x = 7.5;
+			
+			System.out.println( Math.ceil(-Math.abs(-8 + Math.floor(-5.5))));
+	}
+
+}
