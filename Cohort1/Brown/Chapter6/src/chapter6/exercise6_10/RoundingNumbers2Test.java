a94610edcf35c935c0e56d3d2e36228267cd93e1
akinloluolaposi@gmail.com <akinloluolaposi@gmail.com>
2019-06-20 13:46:16 +0100

    Chapter 6 exercises 1.1

---
+++ Cohort1/Brown/Chapter6/src/chapter6/exercise6_10/RoundingNumbers2Test.java
@@ -0,0 +1,16 @@
+package chapter6.exercise6_10;
+
+public class RoundingNumbers2Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RoundingNumbers2 obj = new RoundingNumbers2 ();
+		
+		obj.roundToInteger(5.678);
+		obj.roundToDouble(10.325);
+		obj.roundToHundreth(200.564);
+		obj.roundThousadths(1786.737);
+		
+		
+	}
+}
