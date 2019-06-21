10489e5fc9badbe12909caec649b8374a7ea8268
scoolj <scoolj99@gmail.com>
2019-06-21 13:29:12 +0100

    chapter 6 exercise63

---
+++ Cohort1/Scoolj/chapter6/src/chapter6/exercises63/MathClassVerification.java
@@ -0,0 +1,12 @@
+package chapter6.exercises63;
+
+public class MathClassVerification {
+	
+	public static double calculateMathSquare(double square) {
+		
+		square = Math.sqrt(square);
+		
+		return square;
+	}
+
+}
