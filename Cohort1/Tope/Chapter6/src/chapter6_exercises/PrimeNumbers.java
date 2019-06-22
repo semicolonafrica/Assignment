3404034b737120c26adf4a266b372f47f526030d
omadebaba <iamoyemadeteepee@gmail.com>
2019-06-22 14:28:40 +0100

    Last Commit

--- Cohort1/Tope/Chapter6/src/chapter6_exercises/PrimeNumbers.java
+++ Cohort1/Tope/Chapter6/src/chapter6_exercises/PrimeNumbers.java
@@ -14,7 +14,7 @@ public class PrimeNumbers {
 			
 			if(number % i ==0)
 				primeDeterminant++;
-		}
+			}
 		
 		if(primeDeterminant > 2)
 			primeStatus = false;
@@ -25,19 +25,13 @@ public class PrimeNumbers {
 	}
 	
 	public static void  displayPrimeNumbers() {
-		
 		for(int i = 1; i<=100; i++) {
-			
 			if(isPrime(i) == true)
-				System.out.println(i);
-			
+				System.out.println(i);	
 		}
-		
 	}
 	
 	public static void main(String[] args) {
-		
-		//System.out.println(isPrime(11));
 			displayPrimeNumbers();
 	}
 
