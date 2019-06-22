4e6ef36a7e36d9468bb8f6179f742b7d9fb81854
omadebaba <iamoyemadeteepee@gmail.com>
2019-06-22 23:38:12 +0100

    First commit exercise 6.31

---
+++ Cohort1/Tope/Chapter6/src/chapter6_exercises/ModifyGuessNumberTest.java
@@ -0,0 +1,14 @@
+package chapter6_exercises;
+//Program to test Modify Guess Number class
+
+public class ModifyGuessNumberTest {
+
+	public static void main(String[] args) {
+		
+		ModifyGuessNumber myGame = new ModifyGuessNumber();
+		
+		myGame.playGuessNumber();
+
+	}
+
+}
