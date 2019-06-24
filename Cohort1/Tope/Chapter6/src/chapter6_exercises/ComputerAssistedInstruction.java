fd83f584fbc40b6f9e84048a2c8c2112a1f9541b
omadebaba <iamoyemadeteepee@gmail.com>
2019-06-24 15:26:49 +0100

    Exercise 6.35 & 6.36

--- Cohort1/Tope/Chapter6/src/chapter6_exercises/ComputerAssistedInstruction.java
+++ Cohort1/Tope/Chapter6/src/chapter6_exercises/ComputerAssistedInstruction.java
@@ -4,15 +4,16 @@ import java.security.SecureRandom;
 import java.util.Scanner;
 
 //Application that helps elementary School student learn multiplication
-//Exercise 6.35
+//Exercise 6.35 & 6.36
 //Program written by Oyemade Temitope
 //24th of June 2019
 
 public class ComputerAssistedInstruction {
 	
+	final static SecureRandom random = new SecureRandom();
 	public static int generateQuestions(int answer){
 		
-		SecureRandom random = new SecureRandom();
+		
 		int number1 = 1+ random.nextInt(10);
 		int number2 = 1+ random.nextInt(10);
 		
@@ -25,7 +26,7 @@ public class ComputerAssistedInstruction {
 	
 	public static void askQuestion() {
 		
-		System.out.println("Answer quiz or -1 to end");
+		System.out.println("Answer quiz or -1 to end\n");
 		Scanner input = new Scanner(System.in);
 		int answer = 0;
 		int correctAnswer;
@@ -36,7 +37,7 @@ public class ComputerAssistedInstruction {
 		answer = input.nextInt();
 		
 		if(answer == correctAnswer)
-			System.out.println("Very Good!");
+			System.out.println(generateCommentforCorrectAnswers());
 		
 		else {
 			
@@ -44,11 +45,11 @@ public class ComputerAssistedInstruction {
 				if(answer == -1)
 					break;
 				
-				System.out.println("No. Please try again.");
+				System.out.println(generateCommentforNegativeAnswers());
 				answer = input.nextInt();
 					
 				if(answer == correctAnswer)
-					System.out.println("Very Good!");
+					System.out.println(generateCommentforCorrectAnswers());
 			}
 			
 		}
@@ -60,6 +61,66 @@ public class ComputerAssistedInstruction {
 		
 	}
 	
+	
+	public static String generateCommentforCorrectAnswers() {
+		
+		int number = random.nextInt(4);
+		String remark = " ";
+		String r1 = "Very Good!";
+		String r2 = "Excellent!";
+		String r3 = "Nice Work!";
+		String r4 = "Keep up the good work";
+		
+		switch(number) {
+		case 0:
+			remark = r1;
+			break;
+		case 1:
+			remark = r2;
+			break;
+		case 2:
+			remark = r3;
+			break;
+		case 3:
+			remark = r4;
+			break;
+		
+		}
+		
+		return remark;
+		
+	}
+	
+	
+	public static String generateCommentforNegativeAnswers() {
+		int number = random.nextInt(4);
+		String remark = " ";
+		String r1 = "No! Please try again";
+		String r2 = "Wrong, Try once more";
+		String r3 = "Dont give up!";
+		String r4 = "No! keep trying";
+		
+		switch(number) {
+		case 0:
+			remark = r1;
+			break;
+		case 1:
+			remark = r2;
+			break;
+		case 2:
+			remark = r3;
+			break;
+		case 3:
+			remark = r4;
+			break;
+		
+		}
+		
+		return remark;
+		
+	}
+	
+	//Main method
 	public static void main(String[] args) {
 		
 		askQuestion();
