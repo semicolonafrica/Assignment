92da9359595d7a5bcbc190795bb396762fc32552
damidrevid <oludre@gmail.com>
2019-06-24 10:06:27 +0100

    Greatest Common Divisor and it's implementation.

---
+++ Cohort1/Dami/chapter 6/src/chapter6/chapter6_Group/GreatestCommonDivisor.java
@@ -0,0 +1,17 @@
+package chapter6.chapter6_Group;
+
+public class GreatestCommonDivisor {
+
+	public static int gcd(int a, int b) {
+	
+	    while (a != b) { 
+	        if (a > b) {
+	           a = a - b; 
+	        }
+	        else {
+	           b = b - a; 
+	        }
+	    }
+	    return a;
+			}
+}
