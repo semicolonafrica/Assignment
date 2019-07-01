1481410ac0a42ad069c02453ad67932bfa7b4e61
Oluwatobi lemon <joshval.lj@gmail.com>
2019-07-01 10:22:25 -0700

    more file

--- Chapter6/src/chapter6/exercises/Circle.java
+++ Chapter6/src/chapter6/exercises/Circle.java
@@ -30,6 +30,5 @@ public class Circle {
     }
 	public static void main(String[] args) {
 		circleArea();
-	}
-	
+	}	
 }
--- Chapter6/src/chapter6/exercises/ComputerAssisted3.java
+++ Chapter6/src/chapter6/exercises/ComputerAssisted3.java
@@ -8,25 +8,26 @@ import java.util.Scanner;
  *
  */
 public class ComputerAssisted3 {
-	
+
 	static SecureRandom random = new SecureRandom();
 	static Scanner input = new Scanner(System.in);
-	
-	static int a =0;
-	static int b =0;
-	
-	//method that multiplies
+
+	static int a = 0;
+	static int b = 0;
+
+	// method that multiplies
 	public static int multiply() {
 		a = 1 + random.nextInt(9);
 		b = 1 + random.nextInt(9);
-		int result = a*b;
+		int result = a * b;
 		return result;
 	}
-	//method remark for wrong answers
+
+	// method remark for wrong answers
 	public static void wrongRemark() {
 		int wrong = 1 + random.nextInt(4);
 
-		switch(wrong) {
+		switch (wrong) {
 		case 1:
 			System.out.println("no, please try again");
 			break;
@@ -41,11 +42,11 @@ public class ComputerAssisted3 {
 			break;
 		}
 	}
-	
+
 	public static void rightRemark() {
 		int right = 1 + random.nextInt(4);
 
-		switch(right) {
+		switch (right) {
 		case 1:
 			System.out.println("very good");
 			break;
@@ -60,38 +61,37 @@ public class ComputerAssisted3 {
 			break;
 		}
 	}
-	
-	//method that displays remark
+
+	// method that displays remark
 	public static void displayRemark() {
 //		System.out.println("");
 		int response = 0;
-		int counter =10;
-		double pass =0;
-		for(int i =1;i<=counter; i++) {
+		int counter = 10;
+		double pass = 0;
+		for (int i = 1; i <= counter; i++) {
 			int result = multiply();
-			System.out.println("what is the answer of " + a+ " times "+ b);
+			System.out.println("what is the answer of " + a + " times " + b);
 			response = input.nextInt();
-			if(result != response) {
+			if (result != response) {
 				wrongRemark();
-			}
+			} 
 			else {
 				rightRemark();
 				pass = pass + 1; // store student pass
 			}
 		}
-		
-		double percentage = (pass/counter)*100; // to get student performance
-		if(percentage <75) {
-			System.out.println(percentage +"% Please ask your teacher for extra help");
-			displayRemark();
-		}
-		else {
-			System.out.println(percentage +"% Congratulations, you are ready to go to the next level!");
-			displayRemark();
-		}
-				
-	}
 
+		double percentage = (pass / counter) * 100; // to get student performance
+		if (percentage < 75) {
+			System.out.println(percentage + "% Please ask your teacher for extra help");
+			displayRemark();
+		} 
+		else {
+			System.out.println(percentage + "% Congratulations, you are ready to go to the next level!");
+			displayRemark();
+		}
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
