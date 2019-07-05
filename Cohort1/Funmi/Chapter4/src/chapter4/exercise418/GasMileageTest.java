4f5bb68ce61c28d77c0968384c756a9d77fd98d5
adefunmi94 <oyeyipoadefunmi90@gmail.com>
2019-07-05 10:16:19 +0100

    first commit of Chapter 4

---
+++ Cohort1/Funmi/Chapter4/src/chapter4/exercise418/GasMileageTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package chapter4.exercise418;
+
+/**
+ * @author user
+ *
+ */
+public class GasMileageTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
