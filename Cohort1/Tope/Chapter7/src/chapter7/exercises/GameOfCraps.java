bc2279502ed4be0a6bcdc4d1fd99e3259dc48d1b
omadebaba <iamoyemadeteepee@gmail.com>
2019-07-05 02:38:49 +0100

    First Commit on Exercise 7.16

---
+++ Cohort1/Tope/Chapter7/src/chapter7/exercises/GameOfCraps.java
@@ -0,0 +1,124 @@
+package chapter7.exercises;
+
+import java.security.SecureRandom;
+
+//Exercise 7.16
+//Program written by Oyemade Temitope
+//4th of July 2019
+
+public class GameOfCraps {
+
+	
+
+    private static final SecureRandom randomNumbers = new SecureRandom();
+   
+   private enum Status{CONTINUE, WON, LOST};
+
+  
+   private static final int SNAKE_EYES = 2;
+   private static final int TREY = 3;
+   private static final int SEVEN = 7;
+   private static final int YO_LEVEN = 11;
+   private static final int BOX_CARS = 12;
+   
+    static int[] holdWin = new int[22];
+    static int[] holdLoss = new int[22]; 
+   
+
+   public static int rollDIce(){
+      
+       int die1 = 1+ randomNumbers.nextInt(6); 
+       int die2 = 1+ randomNumbers.nextInt(6);
+
+       int sum = die1 + die2;
+      
+
+       return sum;
+   }
+
+   
+   public static void main(String[] args){
+       int myPoint = 0; 
+       Status gameStatus;
+       
+       for(int i = 1; i<= 1_000_000; i++) {
+    	   
+       int sumOfDice = rollDIce(); 
+
+       
+
+       switch(sumOfDice){
+           case YO_LEVEN:
+           case SEVEN: 
+           gameStatus = Status.WON;
+
+           case SNAKE_EYES:
+           case TREY: 
+           case BOX_CARS: 
+           gameStatus = Status.LOST;
+
+           default:
+           gameStatus = Status.CONTINUE;
+           myPoint = sumOfDice; 
+           break;
+
+       }
+
+       while( gameStatus == Status.CONTINUE){
+      
+           sumOfDice = rollDIce(); 
+        
+           if(sumOfDice == myPoint)
+           gameStatus = Status.WON;
+
+           else if (sumOfDice ==SEVEN)
+           gameStatus = Status.LOST;
+       }
+       
+       if(gameStatus == Status.WON) { 
+    	   if(i <=20)
+    		   ++holdWin[i];
+    	   
+    	   if(i >20)
+    		   ++holdWin[21];
+       }
+       
+       if(gameStatus == Status.LOST) {
+    	   
+    	   if(i <= 20)
+    		   ++holdLoss[i];
+    	   
+    	   if(i>20)
+    		   ++holdLoss[21];
+       }
+       
+   }
+       
+       //Sum Array
+       int u =0;
+       int win= 0;
+       int loss = 0;
+       for(int s =1; s< holdWin.length; s++) {
+    	   
+    	   win = holdWin[s] + holdWin[u];
+    	   holdWin[s] = win;
+    	   
+    	   loss = holdLoss[s] + holdLoss[u];
+    	   holdLoss[s] = loss;
+    	   
+    	   u++;
+       }
+       
+       
+       System.out.printf("%s\t%s\t%s%n", "ROLLS", "NO of WINS", "NO of LOSSES");
+       
+       
+       for(int v = 1; v< holdWin.length; v++) {
+    	   
+    	   System.out.printf("%3d%s\t%5d\t\t%d%n",
+    			   (v <= 20? v :20),(v ==21?'+':""),holdWin[v], holdLoss[v]);
+       }
+   }
+
+
+}
