5c05e4114c41aad843e7ef1c3d9a1297e0e51180
yinkaguti <olayinka2good@yahoo.co.uk>
2019-07-10 16:55:52 +0100

    class work

---
+++ Cohort1/Oloko/Chapter 7/src/chapter7Exercise713/ArraySales.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package chapter7Exercise713;
+
+/**
+ * @author Olayinka Oloko
+ *
+ */
+public class ArraySales {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][]sales = new int [3][5];
+			int counter=0;
+			
+			for (int row = 0; row < sales.length; row++){
+				
+				for (int col = 0; col < sales[row].length; col++){
+			counter++;
+			System.out.printf("%5d%2d",col,sales[row] [col] =0);
+			}
+				System.out.println();
+			}
+   }
+}
+			
+	
+		
+	
+
+
